de1df82b227d8fb63135bae374aac6c775f9c1d8
unknown <azaddoll7@gmail.com>
2023-12-22 12:39:21 +0600

    first commi1t

--- one/az.py
+++ one/az.py
@@ -1,18 +1,18 @@
+
 ''' 
 number = input()
 text = ("your number is {}".format(number))
 print(text)
 '''
-'''
+''' 
 name = input("write your name ")
 age = input(" enter your age ")
 text = ("my name is {}. i am {} years old.".format(name, age))
 print(text)
-
 '''
 number1 = int(input("Enter the first number here")) 
 number2 = int(input("Enter the second number here"))
 
 sum = number1 + number2
 
-print(" entered is {}".format(sum))
+print("The sum of the numbers you entered is {}".format(sum))
